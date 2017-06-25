3ce97b2b5b5201b8eb06c0882b33e36ae045f06c
Gabriel Cedran <gcedran@ciandt.com>
2017-06-25 21:27:12 +0200

    13

--- src/main/java/br/com/cit/desafios/Challenge1.java
+++ src/main/java/br/com/cit/desafios/Challenge1.java
@@ -6,21 +6,21 @@ public class Challenge1 {
 
     public static Article getFirstJavaArticle(List<Article> articles) {
         for (Article article : articles) {
-            if (article.getTags().contains("java11")) {
-                System.out.print("new message from branch 12");
+            if (article.getTags().contains("java13")) {
+                System.out.print("new message from branch 13");
                 return article;
             }
-            System.out.print("counting again");
+            System.out.print("counting");
         }
-        System.out.print("not found");
+        System.out.print("not found!");
         return null;
     }
 
     public static Article getFirstJavaArticle2(List<Article> articles) {
-        return articles.stream().filter(article -> article.getTags().contains("java11")).findFirst().orElse(Article.builder().build());
+        return articles.stream().filter(article -> article.getTags().contains("java13")).findFirst().orElse(Article.builder().build());
     }
 
     public static void print() {
-        System.out.println("new beautiful method - branch 11");
+        System.out.println("new beautiful method - branch 13");
     }
 }
