75ef96243a6eef4851b53461f5c22335e29029b9
Gabriel Cedran <gcedran@ciandt.com>
2017-06-25 20:55:43 +0200

    8

--- src/main/java/br/com/cit/desafios/Challenge1.java
+++ src/main/java/br/com/cit/desafios/Challenge1.java
@@ -6,7 +6,7 @@ public class Challenge1 {
 
     public static Article getFirstJavaArticle(List<Article> articles) {
         for (Article article : articles) {
-            if (article.getTags().contains("java4")) {
+            if (article.getTags().contains("java8")) {
                 System.out.print("message");
                 return article;
             }
@@ -15,6 +15,6 @@ public class Challenge1 {
     }
 
     public static Article getFirstJavaArticle2(List<Article> articles) {
-        return articles.stream().filter(article -> article.getTags().contains("java4")).findFirst().orElse(Article.builder().build());
+        return articles.stream().filter(article -> article.getTags().contains("java8")).findFirst().orElse(Article.builder().build());
     }
 }
